5ec0151b5613bca4c81b86ada265c18e018a88bd
ABPSILV <abpsilva@embraer.com.br>
2023-11-04 16:22:34 -0300

    refact old chains

--- geffen/chain/setup.py
+++ geffen/chain/setup.py
@@ -1,13 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.llms.huggingface_text_gen_inference import HuggingFaceTextGenInference
-from langchain.llms.openllm import OpenLLM
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate, ChatPromptTemplate
-from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.retriever import BaseRetriever
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 
 def setup_prompt(template):
@@ -43,17 +41,11 @@ def hftgi_llm(**kwargs) -> HuggingFaceTextGenInference:
     )
     return llm
 
-def get_llm(llm_type, llm_kwargs) -> OpenLLM:
+def get_llm(llm_type, llm_kwargs):
     if llm_type == "hf":
         return hf_llm(**llm_kwargs)
     else:
         return hftgi_llm(**llm_kwargs)
-
-def llm_chain(llm, template, **kwargs):
-    prompt = setup_prompt(template)
-
-    chain = LLMChain(llm=llm, prompt=prompt, **kwargs)
-    return chain
 
 def runnable_chain(llm, template: str, retriever: BaseRetriever):
     def format_docs(docs):
