94e8535c21d66e8137283578e4aaa8f3e48e49d1
ABPSILV <abpsilva@embraer.com.br>
2023-11-12 10:28:56 -0300

    refact the ETL module using dependency injection

--- geffen/knowledge_base/etl/base.py
+++ geffen/knowledge_base/etl/base.py
@@ -1,4 +1,12 @@
+import uuid
+from tqdm import tqdm
 from typing import Protocol
+
+from langchain.document_loaders.base import BaseLoader
+from langchain.text_splitter import TextSplitter
+
+from knowledge_base.embeddings.types import EmbeddingFunction
+
 
 class KBETL(Protocol):
     
@@ -13,3 +21,27 @@ class KBETL(Protocol):
     
     def load_data(self):
         ...
+
+class KBETLBase(KBETL):
+
+    def extract_data(self, loader:BaseLoader) -> None:
+        self.documents = loader.load()
+    
+    def split_in_chunks(self, splitter: TextSplitter) -> None:
+        self.chunks = splitter.split_documents(self.documents)
+        self.chunks_content = [chunk.page_content for chunk in self.chunks]
+        self.chunks_metadata = [chunk.metadata for chunk in self.chunks]
+    
+    def embed_data(self, embedding_fn: EmbeddingFunction) -> None:
+        self.embeddings = embedding_fn(self.chunks_content)
+        
+    def load_data(self, collection, batch_size: int = 128) -> None:
+        ids = [str(uuid.uuid1()) for _ in range(len(self.chunks))] 
+
+        for i in tqdm(range(0, len(ids), batch_size)):
+            collection.add(
+                documents=self.chunks_content[i:i+batch_size],
+                embeddings=self.embeddings[i:i+batch_size],
+                metadatas=self.chunks_metadata[i:i+batch_size],
+                ids=ids[i:i+batch_size]
+            )
