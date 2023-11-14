4de550b997b4e8055caec3f5ac0d837db499b226
ABPSILV <abpsilva@embraer.com.br>
2023-11-14 11:44:05 -0300

    refact zephyr template

--- geffen/app.py
+++ geffen/app.py
@@ -17,12 +17,14 @@ load_dotenv()
 zephyr_template = '''<|system|>
 You are Geffen, a helpful AI assistant that gives a \
 response to a request based on the following context. \
-Only return the response and nothing more. 
-Context: {context} </s>
+Only return the response and nothing more.
+Context: {context}</s>
 <|user|>
-Request: {request} </s>
+Request: {request}</s>
 <|assistant|>
-Response: '''
+Response:'''
+
+
 
 db_config = KBConfig(
         host=os.getenv("DB_HOST"),
